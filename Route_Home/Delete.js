47b755a66095a0e91cdcc783101c20a9be00f3b2
Masri Alemu <masrialemu@gmail.com>
2023-06-04 00:38:57 +0300

    update40

--- Route_Home/Delete.js
+++ Route_Home/Delete.js
@@ -3,27 +3,35 @@ const router = express.Router();
 const User = require('../Mongoose/Post');
 const Auth = require('../Mongoose/Auth');
 const Token = require('../Token');
-const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
+
+// async function deleteImageFromImgBB(imageUrl) {
+//   try {
+//     const urlParts = imageUrl.split('/');
+//     const imageId = urlParts[urlParts.length - 1].split('.')[0];
+//     const response = await axios.delete(`https://api.imgbb.com/1/image/${imageId}?key=bb3c04e726776d171fb92035dfb747cf`);
+
+//     // Check if the delete request was successful
+//     if (response.data.status_code === 200) {
+//       console.log('Image deleted successfully.');
+//     } else {
+//       console.error('Failed to delete image:', response.data.error);
+//     }
+//   } catch (error) {
+//     console.error('Error deleting image from ImgBB:', error.message);
+//     throw error;
+//   }
+// }
 
 router.delete('/:id', Token, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
 
     if (user.email === req.userId.email || req.userId.isAdmin) {
-      // Delete the user's post
-      await User.findByIdAndDelete(req.params.id);
 
-      // Delete the associated image file
-      if (user.public_url) {
-        // Extract the file name from the public_url
-        const fileName = path.basename(user.public_url);
-        
-        // Delete the previous image file if it exists
-        fs.unlinkSync(`Pic/${fileName}`);
-      }
+      await User.deleteOne({ _id: req.params.id });
 
-      return res.status(200).json({ user, message: 'User is deleted' });
+      return res.status(200).json({ user, message: 'User and associated files are deleted' });
     }
 
     return res.status(404).json({ message: 'User not found' });
